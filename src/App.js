74f36825daee67d56ab7051cab0aae35e10740fe
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-28 16:35:55 +0530

    Understand Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import ClickCounter from "./components/ClickCounter";
 import FocusInput from "./components/FocusInput";
 import Form from "./components/Form";
 import FragmentDemo from "./components/FragmentDemo";
+import HoverComponent from "./components/HoverComponent";
 import LifeCycleA from "./components/LifeCycleA";
 import RefsDemo from "./components/RefsDemo";
 import Table from "./components/Table";
@@ -9,7 +11,9 @@ import Table from "./components/Table";
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <ClickCounter />
+      <HoverComponent />
+      {/* <FocusInput /> */}
       {/* <RefsDemo /> */}
       {/* <Table /> */}
       {/* <FragmentDemo /> */}
