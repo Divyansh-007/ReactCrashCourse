0eb7acb1f52e1e682c82fecf5842b934278bc244
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-29 16:51:13 +0530

    Understand render props pattern similiar to HOC pattern

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
 import ClickCounter from "./components/ClickCounter";
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import Counter from "./components/Counter";
 import FocusInput from "./components/FocusInput";
 import Form from "./components/Form";
 import FragmentDemo from "./components/FragmentDemo";
 import HoverComponent from "./components/HoverComponent";
+import HoverComponentTwo from "./components/HoverComponentTwo";
 import LifeCycleA from "./components/LifeCycleA";
 import RefsDemo from "./components/RefsDemo";
 import Table from "./components/Table";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
-      <ClickCounter name="Divyansh" />
-      <HoverComponent name="DarkKnight" />
+      <Counter
+        render={(count, incrementCount) => {
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />;
+        }}
+      />
+      <Counter
+        render={(count, incrementCount) => {
+          <HoverComponentTwo count={count} incrementCount={incrementCount} />;
+        }}
+      />
+      {/* <ClickCounterTwo /> */}
+      {/* <HoverComponentTwo /> */}
+      {/* <User render={(isLoggedIn) => (isLoggedIn ? "Divyansh" : "Guest")} /> */}
+      {/* <ClickCounter name="Divyansh" /> */}
+      {/* <HoverComponent name="DarkKnight" /> */}
       {/* <FocusInput /> */}
       {/* <RefsDemo /> */}
       {/* <Table /> */}
